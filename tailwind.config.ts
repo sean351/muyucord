f46ee002b5d20d5d9652ddeac1f87f6467b8ad00
Sean Doran <sdoran35@gmail.com>
2024-02-24 16:13:29 -0500

    init project

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        discord_blue: "#295DE7",
+        discord_blurple: "#7289da",
+        discord_purple: "#5865f2",
+        discord_green: "#3ba55c",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
+export default config;
