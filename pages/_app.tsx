827ad469e4aef9148f8b6ac93d938d2d7362934b
Sean Doran <sdoran35@gmail.com>
2024-02-24 17:33:26 -0500

    changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <title>My awesome PWA app</title>
-        <meta name="description" content="Best PWA app in the world!" />
+        <meta name="description" content="Muyu Cord!" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="mask-icon" href="/icons/mask-icon.svg" color="#FFFFFF" />
         <meta name="theme-color" content="#ffffff" />
@@ -30,18 +30,6 @@ export default function App({ Component, pageProps }: AppProps) {
           href="/icons/touch-icon-ipad-retina.png"
         />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content="https://yourdomain.com" />
-        <meta name="twitter:title" content="My awesome PWA app" />
-        <meta name="twitter:description" content="Best PWA app in the world!" />
-        <meta name="twitter:image" content="/icons/twitter.png" />
-        <meta name="twitter:creator" content="@DavidWShadow" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="My awesome PWA app" />
-        <meta property="og:description" content="Best PWA app in the world!" />
-        <meta property="og:site_name" content="My awesome PWA app" />
-        <meta property="og:url" content="https://yourdomain.com" />
-        <meta property="og:image" content="/icons/og.png" />
         {/* add the following only if you want to add a startup image for Apple devices. */}
         <link
           rel="apple-touch-startup-image"
